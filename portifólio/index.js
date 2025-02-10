b5e80eaa635ee29d1f53d374fc2a86066b58f3c9
Devpdias <pedrohenriqueherculanodias@gmail.com>
2025-02-10 16:32:27 -0300

    page

--- portifólio/index.js
+++ portifólio/index.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", () => {
     const observer = new IntersectionObserver(
         (entries) => {
@@ -11,27 +12,23 @@ document.addEventListener("DOMContentLoaded", () => {
             threshold: 0.1,
         }
     );
-
+//navbar sumir 
 
     const hiddenElements = document.querySelectorAll(".hidden");
     hiddenElements.forEach((el) => observer.observe(el));
 });
 
-// script.js
-
 let lastScrollY = window.scrollY;
 const header = document.querySelector('header');
 
-// Detecta a rolagem da página
+
 window.addEventListener('scroll', () => {
     const currentScrollY = window.scrollY;
 
     if (currentScrollY > lastScrollY) {
-        // Rola para baixo: esconde o header
         header.classList.add('hidden');
         header.classList.remove('show');
     } else {
-        // Rola para cima: mostra o header
         header.classList.add('show');
         header.classList.remove('hidden');
     }
@@ -39,13 +36,11 @@ window.addEventListener('scroll', () => {
     lastScrollY = currentScrollY;
 });
 
-// Mostra o header ao passar o mouse
 header.addEventListener('mouseenter', () => {
     header.classList.add('show');
     header.classList.remove('hidden');
 });
 
-// Esconde o header ao sair com o mouse, somente se estiver rolando para baixo
 header.addEventListener('mouseleave', () => {
     if (window.scrollY > lastScrollY) {
         header.classList.add('hidden');
